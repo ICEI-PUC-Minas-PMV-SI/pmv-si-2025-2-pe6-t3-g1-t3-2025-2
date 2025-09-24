3c3a8d17e354faab60a2feeafa41949f22ff8d55
Andre Murilo <andremurilovf@gmail.com>
2025-09-24 00:09:28 -0300

    Fix: DTO's usuário com obrigatoriedade de email, nome e senha ao criar / login

--- HotelFazendaApi/HotelFazendaApi/DTOs/CreateUserDto.cs
+++ HotelFazendaApi/HotelFazendaApi/DTOs/CreateUserDto.cs
@@ -4,8 +4,8 @@ namespace HotelFazendaApi.DTOs
 {
     public class CreateUserDto
     {
-        public string? Name { get; set; }
-        public string? Email { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
         public string? Password { get; set; }
         public Role Role { get; set; }
     }
